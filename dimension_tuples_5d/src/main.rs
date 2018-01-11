3681edba5aed697c416d763c959ba741dead84cd
Nikolaj von Holck <mail@nikolajholck.dk>
2018-01-11 01:40:01 +0100

    Rearrange non-trivial overlap inequalities.

--- dimension_tuples_5d/src/main.rs
+++ dimension_tuples_5d/src/main.rs
@@ -43,11 +43,11 @@ fn representative_tuples_4d(limit: usize) {
 
 fn representative_tuples_5d(limit: usize) {
     let overlap_inequalities = [
-        ([3, 4], [2, 5]),
-        ([3, 4], [1, 5]),
-        ([2, 4], [1, 5]),
+        ([1, 4], [2, 3]),
         ([2, 3], [1, 5]),
-        ([2, 3], [1, 4])
+        ([1, 5], [2, 4]),
+        ([1, 5], [3, 4]),
+        ([2, 5], [3, 4])
     ];
     let mut counts: HashMap<String, usize> = HashMap::new();
     for a in 1..limit {
